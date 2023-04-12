b3fba2cfb4bd2dd5dd9412fb5c575532fe4e818a
Marcos <marcos.attwell@gmail.com>
2023-04-12 09:56:42 -0300

    version de prueba ej2

--- ejercicio2Parca/main.cpp
+++ ejercicio2Parca/main.cpp
@@ -1,41 +1,45 @@
+/* 
+ * m<=10^5
+ * n<=10^5
+ * m*n<=10^7
+ */
 #include <bits/stdc++.h>
 using namespace std;
 
-enum tristate {
-    UNDEFINED,
-    FALSE,
-    TRUE
-};
-
-bool esEntero(float x) {
-    return ceilf(x) == x;
-}
-
-float raizN(float radicando, float indice) {
-    return pow(radicando, 1/indice);
-}
-
-vector<int> v = {3,1,5,2,1};
+// Donde memo(i, m) -> true/false, para 0 <= i < n, 0 <= m < M(m original)
 map<pair<int, int>, bool> memo;
 
-bool solve(int i, float w, vector<int>& v) {
-    if (!esEntero(w)) return false;
-    if (i == 0) return (w - v[i]) == 0;
+int aPowbModc(int a, int b, int c) { // Hago esto pq sino la potencia se puede ir a un número gigantesco
+    a = a % c;
+    long long int res = 1;
+    while (b > 0) {
+        res = (res * a) % c; // Gracias por tanto profes de Álgebra I :)
+        b--;
+    }
+    return res;
+}
+
+bool solve(int i, int w, int n, int r, int m, vector<int>& v) {
+    if (i == n) return (((w+r) % m) == r);
     pair<int, int> par = make_pair(i, w);
     if (memo.count(par) == 0) {
-        bool res = solve(i-1, w - v[i], v) || solve(i-1, w / v[i], v) || solve(i-1, raizN(w, v[i]), v);
+        bool res =  solve(i+1, w - v[i], n, r, m, v) ||
+                    solve(i+1, w * v[i], n, r, m, v) ||
+                    solve(i+1, aPowbModc(w, v[i], m), n, r, m, v) ||
+                    solve(i+1, w + v[i], n, r, m, v);
         memo.insert(make_pair(par, res));
     }
     return memo.at(par);
-    //return solve(i-1, w - v[i], v) || solve(i-1, w / v[i], v) || solve(i-1, raizN(w, v[i]), v);
 }
 
 int main() {
-    int w = 400;
-    cout << "got:      " << (solve(v.size()-1, w, v) ? "true" : "false") << endl;
-    cout << "expected: true" << endl;
-    vector<int> a = {4,5};
-    int b = 21;
-    cout << "got:      " << (solve(a.size()-1, b, a) ? "true" : "false") << endl;
-    cout << "expected: false" << endl;
+    int c; cin >> c;
+    while (c--) {
+        memo.clear();
+        int n, r, m;
+        std::cin >> n >> r >> m;
+        vector<int> v(n, 0);
+        for (int i=0; i < n; i++) std::cin >> v[i];
+        cout << (solve(0, 0, n, r, m, v) ? "Si" : "No") << endl;
+    }
 }
