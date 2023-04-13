0ae6c58100d0de0a88c7b9d71a16acc9c27c4799
Jorge Szabo <jorgecszabo@gmail.com>
2023-04-12 22:22:39 -0300

    Pasa el ej2 VAMOS CARAJOOO!!

--- vectorDeNatsJorge/main.cpp
+++ vectorDeNatsJorge/main.cpp
@@ -3,47 +3,63 @@
 
 using namespace std;
 
-int aPowbModc(int a, int b, int c) { // Hago esto pq sino la potencia se puede ir a un número gigantesco
-    a = a % c;
-    long long int res = 1;
-    while (b > 0) {
-        res = (res * a) % c; // Gracias por tanto profes de Álgebra I :)
-        b--;
+vector<unsigned long int> v;
+int n;
+int r;
+int m;
+
+enum tristate {
+    UNDEFINED,
+    FALSE,
+    TRUE
+};
+
+vector<vector<tristate>> cache;
+
+int aModb(long int a, int b) {
+	return ((a % b) + b) % b;
+}
+
+int aPowbModc(unsigned long int a, int b, int c) { // Hago esto pq sino la potencia se puede ir a un número gigantesco
+    if (b == 0) return 1;
+    a = aModb(a, c);
+    if (b % 2 == 0) {
+    	return aPowbModc((a * a) % c, b/2, c) % c;
     }
+    else return (a * aPowbModc(a, b-1, c)) % c;
+}
+
+bool canReachTarget(int i, unsigned long int prevRes) {
+    if (i == n) return prevRes == r;
+    if (cache[i][prevRes] != UNDEFINED) return cache[i][prevRes] == TRUE;
+    bool res = canReachTarget(i+1, (prevRes + v[i]) % m) ||
+    canReachTarget(i+1, (prevRes * v[i]) % m) ||
+    canReachTarget(i+1, aPowbModc(prevRes, v[i], m)) ||
+    canReachTarget(i+1, aModb(prevRes - v[i], m));
+    if (res == true) cache[i][prevRes] = TRUE;
+    else cache[i][prevRes] = FALSE;
     return res;
 }
 
-bool canReachTarget(int i, vector<bool> &cachePrev, vector<bool> &cacheCurr, const vector<int> &v, int n, int m , int r) {
-    cachePrev[m + (v[i] + v[i+1]) % m] = true;
-    cachePrev[m + (v[i] - v[i+1]) % m] = true;
-    cachePrev[m + (v[i] * v[i+1]) % m] = true;
-    cachePrev[m + aPowbModc(v[i], v[i+1], m)] = true;
-    i += 2;
-    while (i < n) {
-        for (int idx = 0; idx < 2*m; idx++) cacheCurr[idx] = false;
-        for (int idx = 0; idx < 2*m; idx++) {
-            if (cachePrev[idx]) {
-                int prevRes = idx - m; // Para poder representar valores negativos
-                cacheCurr[m + ((prevRes + v[i]) % m)] = true;
-                cacheCurr[m + ((prevRes - v[i]) % m)] = true;
-                cacheCurr[m + ((prevRes * v[i]) % m)] = true;
-                cacheCurr[m + aPowbModc(prevRes, v[i], m)] = true;
-            }
-        }
-        swap(cachePrev, cacheCurr); // ES O(1) según la documentación, solo mueve punteros
-        i++;
-    }
-    return cachePrev[m + r];
-}
-
 int main() {
-    vector<int> v = {4,4,7};
-    const int n = v.size();
-    int r = 27936; // (4^4^7 mod 100000)
-    const int m = 100000;
+    int problemNumber = 0;
+    cin >> problemNumber;
+    while (problemNumber > 0) {
+        cin >> n >> r >> m;
+        v = vector<unsigned long int>(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        if (r >= m) {
+        	cout << "No" << endl;
+        	problemNumber++;
+        	continue;
+        }
+        cache = vector<vector<tristate>>(n, vector<tristate>(m, UNDEFINED));
+        if (canReachTarget(1, v[0])) cout << "Si" << endl;
+        else cout << "No" << endl;
+        problemNumber--;
+    }
 
-    vector<bool> cacheCurr(2*m); // Igual me lo limpia el ciclo
-    vector<bool> cachePrev(2*m, false);
-    std::cout << canReachTarget(0, cachePrev, cacheCurr, v, n, m, r) << std::endl;
     return 0;
 }
