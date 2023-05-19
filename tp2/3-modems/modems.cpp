d195475889a1a876b29aaa150ab14d5a866679ca
Jorge Szabo <jorgecszabo@gmail.com>
2023-05-19 02:15:41 -0300

    modems funcionando con juez

---
+++ tp2/3-modems/modems.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const ll inf = 1e18;
+
+double distancia(const tuple<int, int> &p1, const tuple<int, int> &p2) { // <x,y>
+    return sqrt((get<0>(p2)-get<0>(p1))*(get<0>(p2)-get<0>(p1)) + (get<1>(p2)-get<1>(p1))*(get<1>(p2)-get<1>(p1)));
+}
+
+bool tercerElemento(const tuple<int, int, double>& a,
+                    const tuple<int, int, double>& b) {
+    return (get<2>(a) < get<2>(b));
+}
+
+struct DSU{
+
+    DSU(int n){
+        padre = rank = vector<int>(n);
+        for(int v = 0; v < n; v++) padre[v] = v;
+    }
+
+    int find(int v){
+        if(v == padre[v]) return v;
+        return padre[v] = find(padre[v]);
+    }
+
+    void unite(int u, int v){
+        u = find(u), v = find(v);
+        if(u == v) return;
+        if(rank[u] < rank[v]) swap(u,v);
+        padre[v] = padre[u];
+        rank[u] = max(rank[u],rank[v]+1);
+    }
+
+    vector<int> padre;
+    vector<int> rank;
+};
+
+tuple<double, double> solve(vector<tuple<int,int,double>> &listaAristas, int n, int cantModem, int costoUTP, int limitUTP, int costoFibra) {
+    sort(listaAristas.begin(),listaAristas.end(), tercerElemento);
+    int tamBosque = n;
+    double acumUTP = 0;
+    double acumFibra = 0;
+    DSU dsu(n);
+    for(auto [u, v, d] : listaAristas){
+        //si (u,v) es arista segura
+        if(dsu.find(u) != dsu.find(v)){
+            dsu.unite(u,v);
+            tamBosque--;
+            if (d > (double) limitUTP) acumFibra += d * costoFibra;
+            else acumUTP += d * costoUTP;
+        }
+        if (tamBosque == cantModem) break;
+    }
+    return make_tuple(acumUTP, acumFibra);
+}
+
+int main() {
+    int casos;
+    cin >> casos;
+    for (int caso = 1; caso <= casos; caso++) {
+        int n, limitUTP, cantModems, precioUTP, precioFibra;
+        cin >> n >> limitUTP >> cantModems >> precioUTP >> precioFibra;
+        int input = n;
+        vector<tuple<int,int>> oficinas;
+        vector<tuple<int,int,double>> listaAdj; // <u, v, peso>
+        while (input--) {
+            int x, y;
+            cin >> x >> y;
+            oficinas.emplace_back(x,y);
+        }
+
+        // genero la lista de adyacencias del grafo pesado. Va a ser un grafo completo.
+        for (int i = 0; i < oficinas.size() - 1; i++) {
+            for (int j = i + 1; j < oficinas.size(); j++) {
+                listaAdj.emplace_back(i, j, distancia(oficinas[i], oficinas[j]));
+                listaAdj.emplace_back(j, i, distancia(oficinas[i], oficinas[j]));
+            }
+        }
+        tuple<double, double> res = solve(listaAdj, n, cantModems, precioUTP, limitUTP, precioFibra);
+        cout << setprecision(3) << fixed;
+        cout << "Caso #" << caso << ": ";
+        cout << get<0>(res) << " " << get<1>(res) << endl;
+    }
+    return 0;
+}
